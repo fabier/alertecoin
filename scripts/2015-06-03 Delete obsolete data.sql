a21257fb38e78c832fea0b7b1024d0c5955d4314
Pierre FABIER <fabier@free.fr>
2015-06-03 20:50:23 +0200

    Ajout script suppression de données obsolete en base

---
+++ scripts/2015-06-03 Delete obsolete data.sql
@@ -0,0 +1,24 @@
+CREATE TEMP TABLE tempImages AS
+SELECT I.id FROM image I
+LEFT JOIN classified_image CI
+ON CI.image_id = I.id
+WHERE CI.classified_images_id IS NULL;
+
+DELETE FROM image USING tempImages
+WHERE image.id = tempImages.id;
+
+DROP TABLE tempImages;
+
+CREATE TEMP TABLE tempClassified AS
+SELECT C.id FROM classified C
+LEFT JOIN alert_classified AC
+ON AC.classified_id = C.id
+WHERE AC.classified_id IS NULL;
+
+DELETE FROM classified_image USING tempClassified
+WHERE classified_image.classified_images_id = tempClassified.id;
+
+DELETE FROM classified USING tempClassified
+WHERE classified.id = tempClassified.id;
+
+DROP TABLE tempClassified;
