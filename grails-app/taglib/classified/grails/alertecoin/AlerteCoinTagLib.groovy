36171e752e0526ae9078f48de00834265ef332fe
Pierre FABIER <pierre.fabier@gmail.com>
2015-09-06 16:09:02 +0200

    Ajout de la tablib qui permet de récupérer une valeur d'extra en fonction de sa clé

---
+++ grails-app/taglib/classified/grails/alertecoin/AlerteCoinTagLib.groovy
@@ -0,0 +1,21 @@
+package classified.grails.alertecoin
+
+import com.pfabier.alertecoin.ClassifiedExtra
+
+class AlerteCoinTagLib {
+    static defaultEncodeAs = [taglib: 'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+
+    def getExtra = { attrs, body ->
+        assert attrs.extras
+        assert attrs.name
+
+        List<ClassifiedExtra> classifiedExtras = attrs.extras
+        ClassifiedExtra classifiedExtra = classifiedExtras.find {
+            it.key.name == attrs.name
+        }
+        if (classifiedExtra != null) {
+            out << raw(classifiedExtra.value)
+        }
+    }
+}
