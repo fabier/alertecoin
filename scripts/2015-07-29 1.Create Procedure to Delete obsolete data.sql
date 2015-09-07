1e3ec6e5dc81d57e1fc8fc4b9b815fb44685a6f5
Pierre FABIER <pierre.fabier@gmail.com>
2015-09-08 01:19:37 +0200

    Mise à jour des procédures de suppression des données inutiles

--- scripts/2015-07-29 1.Create Procedure to Delete obsolete data.sql
+++ scripts/2015-07-29 1.Create Procedure to Delete obsolete data.sql
@@ -27,6 +27,22 @@ WHERE AC.classified_id IS NULL;
 DELETE FROM classified_image USING tempClassified
 WHERE classified_image.classified_images_id = tempClassified.id;
 
+CREATE TEMP TABLE classified_extra_to_delete
+(id BIGINT);
+
+INSERT INTO classified_extra_to_delete(id)
+SELECT CCE.classified_extra_id FROM classified_classified_extra CCE
+INNER JOIN tempClassified TC
+ON CCE.classified_classified_extras_id = TC.id;
+
+DELETE FROM classified_classified_extra USING tempClassified
+WHERE classified_classified_extra.classified_classified_extras_id = tempClassified.id;
+
+DELETE FROM classified_extra USING classified_extra_to_delete
+WHERE classified_extra.id = classified_extra_to_delete.id;
+
+DROP TABLE classified_extra_to_delete;
+
 DELETE FROM classified USING tempClassified
 WHERE classified.id = tempClassified.id;
 
