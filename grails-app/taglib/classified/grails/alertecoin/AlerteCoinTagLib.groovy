3869f940da87cd7385ce03792a76fdec54e11220
Pierre FABIER <pierre.fabier@gmail.com>
2015-09-08 00:45:07 +0200

    Suppression de l'erreur dans le cas om l'annonce n'a pas d'extra

--- grails-app/taglib/classified/grails/alertecoin/AlerteCoinTagLib.groovy
+++ grails-app/taglib/classified/grails/alertecoin/AlerteCoinTagLib.groovy
@@ -7,15 +7,14 @@ class AlerteCoinTagLib {
     //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
 
     def getExtra = { attrs, body ->
-        assert attrs.extras
-        assert attrs.name
-
-        List<ClassifiedExtra> classifiedExtras = attrs.extras
-        ClassifiedExtra classifiedExtra = classifiedExtras.find {
-            it.key.name == attrs.name
-        }
-        if (classifiedExtra != null) {
-            out << raw(classifiedExtra.value)
+        if (attrs.extras && attrs.name) {
+            List<ClassifiedExtra> classifiedExtras = attrs.extras
+            ClassifiedExtra classifiedExtra = classifiedExtras?.find {
+                it.key.name == attrs.name
+            }
+            if (classifiedExtra != null) {
+                out << raw(classifiedExtra.value)
+            }
         }
     }
 }
