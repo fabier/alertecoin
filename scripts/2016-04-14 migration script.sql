a7c68162828b3773ab16e63193c4a3615ac872cf
Pierre FABIER <pierre.fabier@gmail.com>
2016-04-14 10:27:32 +0200

    Passage en https

--- scripts/2016-04-14 migration script.sql
+++ scripts/2016-04-14 migration script.sql
@@ -1,3 +1,7 @@
 ALTER TABLE classified_classified_extra RENAME classified_classified_extras_id TO classified_id;
 
 ALTER TABLE alert ALTER COLUMN description TYPE text;
+
+UPDATE alert
+SET url= replace(url, 'http://', 'https://')
+WHERE url like '%http://%';
