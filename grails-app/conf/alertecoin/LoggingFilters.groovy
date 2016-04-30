4f2ce3cc1eb4417e0ad9e3a845a3b8c66a79d04e
Pierre FABIER <pierre.fabier@gmail.com>
2016-04-30 10:05:02 +0200

    Ne pas logguer les appels nagios

--- grails-app/conf/alertecoin/LoggingFilters.groovy
+++ grails-app/conf/alertecoin/LoggingFilters.groovy
@@ -6,7 +6,12 @@ class LoggingFilters {
         all(controller: '*', action: '*') {
             before = {
                 def remoteAddr = request.getRemoteAddr()
-                log.info "controller:$controllerName action:$actionName -> $params (from ${remoteAddr})"
+                if ("127.0.0.1".equals(remoteAddr) && actionName.equals("index")) {
+                    // Acces depuis nagios certainement pour vérifer le bon fonctionnement du service
+                    // On ne loggue pas
+                } else {
+                    log.info "controller:$controllerName action:$actionName -> $params (from IP: ${remoteAddr})"
+                }
             }
             after = { Map model ->
 
