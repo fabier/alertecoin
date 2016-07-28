46bebfbb356b4e4d29e5b6c660abba8a619d5bca
Pierre FABIER <pierre.fabier@gmail.com>
2016-07-28 11:23:01 +0200

    Ajout de logs d'accès

--- grails-app/conf/alertecoin/LoggingFilters.groovy
+++ grails-app/conf/alertecoin/LoggingFilters.groovy
@@ -1,16 +1,36 @@
 package alertecoin
 
+import log.AccessLog
+
 class LoggingFilters {
 
     def filters = {
-        all(controller: '*', action: '*') {
+        allButImage(controller: 'image', invert: true) {
             before = {
                 def remoteAddr = request.getRemoteAddr()
-                if ("127.0.0.1".equals(remoteAddr) && actionName.equals("index")) {
-                    // Acces depuis nagios certainement pour vérifer le bon fonctionnement du service
-                    // On ne loggue pas
-                } else {
-                    log.info "controller:$controllerName action:$actionName -> $params (from IP: ${remoteAddr})"
+                boolean shouldLogToDatabase = true
+                boolean shouldLogToFile = true
+                if ("127.0.0.1".equals(remoteAddr) || "0:0:0:0:0:0:0:1".equals(remoteAddr)) {
+                    // On n'enregistre pas en base
+                    shouldLogToDatabase = false
+                    if (controllerName.equals("common") && actionName.equals("index")) {
+                        // Acces depuis nagios certainement pour vérifer le bon fonctionnement du service
+                        // On ne loggue pas sur fichier
+                        shouldLogToFile = false
+                    }
+                }
+
+                if (shouldLogToFile) {
+                    log.info "${request.forwardURI} -> $params (${remoteAddr})"
+                }
+                if (shouldLogToDatabase) {
+                    new AccessLog(
+                            ip: remoteAddr,
+                            path: request.forwardURI,
+                            controller: controllerName,
+                            action: actionName,
+                            queryString: request.queryString
+                    ).save()
                 }
             }
             after = { Map model ->
