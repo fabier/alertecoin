8e67559b1f0f52bd64e661378006ac68a1746e1e
Pierre FABIER <fabier@free.fr>
2017-04-23 10:02:51 +0200

    Insérer entre 2 et 10s entre deux appels à lbc

--- grails-app/controllers/alertecoin/ScanController.groovy
+++ grails-app/controllers/alertecoin/ScanController.groovy
@@ -9,12 +9,11 @@ import java.util.concurrent.locks.ReentrantLock
 @Secured(['permitAll'])
 class ScanController {
 
-    MailService mailService
     AlertService alertService
     NetworkService networkService
     UserService userService
 
-    ReentrantLock reentrantLock = new ReentrantLock()
+    ReentrantLock scanLock = new ReentrantLock()
 
     def beforeInterceptor = {
         boolean isRequestFromLocalhost = networkService.isRequestFromLocalhost(request)
@@ -30,79 +29,38 @@ class ScanController {
     }
 
     def index() {
-        def remoteAddr = request.getRemoteAddr()
-        log.debug "remoteAddr : $remoteAddr"
+        // Méthode synchronisée pour éviter que plusieurs thread entrent en concurrence
+        if (scanLock.tryLock(10, TimeUnit.SECONDS)) {
+            try {
+                // On vérifie si au moins une recherche n'est pas en souffrance...
+                // On scanne d'abord les alertes qui n'ont jamais été lancées
+                List<Alert> alerts = Alert.findAllByLastCheckedDateIsNull([max: 10])
 
-        Enumeration e = NetworkInterface.getNetworkInterfaces();
-        Set<String> localIPAddresses = new HashSet<>()
-        while (e.hasMoreElements()) {
-            NetworkInterface n = (NetworkInterface) e.nextElement();
-            Enumeration ee = n.getInetAddresses();
-            while (ee.hasMoreElements()) {
-                InetAddress i = (InetAddress) ee.nextElement();
-                localIPAddresses.add(i.getHostAddress());
-            }
-        }
-
-        // On verrouille l'usage : doit être appelé par la machine hébergeant le service.
-        if ("127.0.0.1".equals(remoteAddr) || "0:0:0:0:0:0:0:1".equals(remoteAddr) || localIPAddresses.contains(remoteAddr)) {
-            // Méthode synchronisée pour éviter que plusieurs thread entrent en concurrence
-            if (reentrantLock.tryLock(1, TimeUnit.SECONDS)) {
-                try {
-                    // On vérifie si au moins une recherche n'est pas en souffrance...
-                    // On scanne d'abord les alertes qui n'ont jamais été lancées
-                    List<Alert> alerts = Alert.findAll("from Alert as A WHERE A.lastCheckedDate IS NULL", [max: 10])
-
-                    if (alerts == null || alerts.isEmpty()) {
-                        // On scanne ensuite les alertes qui n'ont pas encore de date de "prochaine vérification"
-                        alerts = Alert.findAll("from Alert as A WHERE A.nextCheckDate IS NULL", [max: 10])
-                    }
+                if (alerts == null || alerts.isEmpty()) {
+                    // On scanne ensuite les alertes qui n'ont pas encore de date de "prochaine vérification"
+                    alerts = Alert.findAllByNextCheckDateIsNull([max: 10])
 
                     if (alerts == null || alerts.isEmpty()) {
                         // On vérifie finalement les alertes dans leur ordre d'execution programmée par la date de prochaine execution
-                        alerts = Alert.findAll("from Alert as A order by A.nextCheckDate ASC", [max: 10])
+                        alerts = Alert.findAllByNextCheckDateLessThanEquals(new Date(), [max: 10, sort: "nextCheckDate", order: "asc"])
                     }
-
-                    if (alerts != null || !alerts.isEmpty()) {
-                        for (Alert alert : alerts) {
-                            if (alert != null) {
-                                String delayAsString = "N/A"
-                                User user = User.get(alert.userId)
-                                boolean hasToCheck
-
-                                if (alert?.nextCheckDate != null) {
-                                    long now = System.currentTimeMillis()
-                                    long alertCheckTime = alert.nextCheckDate.getTime()
-                                    if (now > alertCheckTime) {
-                                        hasToCheck = true
-                                        Date delayDate = new Date(now - alertCheckTime)
-                                        delayAsString = delayDate.format("mm 'min' ss 's'")
-                                    } else {
-                                        // Pas d'alerte à vérifier
-                                        hasToCheck = false
-                                    }
-                                } else {
-                                    hasToCheck = true
-                                }
-
-                                if (hasToCheck) {
-                                    log.info "Scanning : [${alert.id}] ${alert.name} (${user.displayName ?: user.email})"
-                                    alertService?.fillWithNewClassifiedsAndSendEmailIfNewFound(alert)
-                                }
-                            }
-                        }
-                    }
-                } finally {
-                    reentrantLock.unlock()
                 }
-                render text: "scan : OK"
-            } else {
-                // Tant pis, on essaiera plus tard
-                render text: "scan : Locked, retry later..."
+
+                if (alerts != null && !alerts.isEmpty()) {
+                    for (Alert alert : alerts) {
+                        User user = User.get(alert.userId)
+                        log.info "Scanning : [${alert.id}] ${alert.name} (${user.displayName ?: user.email})"
+                        alertService.fillWithNewClassifiedsAndSendEmailIfNewFound(alert)
+                        Thread.sleep(2000 + (Math.random() * 8000)) // Attendre entre 2s et 10s
+                    }
+                }
+            } finally {
+                scanLock.unlock()
             }
+            render text: "scan : OK"
         } else {
-            log.warn("Trying to access /rankingHistory/populateNext from IP : ${remoteAddr}")
-            response.sendError(404)
+            // Tant pis, on essaiera plus tard
+            render text: "scan : Locked, retry later..."
         }
     }
 }
